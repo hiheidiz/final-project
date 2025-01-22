d486e79349ac61d786154ebb0cfec7d9de4e78fe
heidiz05 <162917691+heidiz05@users.noreply.github.com>
2025-01-22 17:19:39 -0500

    build

--- client/src/components/pages/Profile.jsx
+++ client/src/components/pages/Profile.jsx
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-// import CatHappiness from "../modules/CatHappiness";
-import { get, post } from "../../utilities";
+import { get } from "../../utilities";
 import "./Profile.css";
 
 const Profile = (props) => {
-  // const [catHappiness, setCatHappiness] = useState(0);
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -19,22 +17,13 @@ const Profile = (props) => {
     });
   }, []);
 
-  // const incrementCatHappiness = () => {
-  //   setCatHappiness(catHappiness + 1);
-  // };
-
   return (
     <>
       {!user ? (
         <div> Loading! </div>
       ) : (
         <>
-          <div
-            className="Profile-avatarContainer"
-            // onClick={() => {
-            //   incrementCatHappiness();
-            // }}
-          >
+          <div className="Profile-avatarContainer">
             <div className="Profile-avatar" />
           </div>
           <h1 className="Profile-name u-textCenter">{user.name}</h1>
@@ -43,12 +32,8 @@ const Profile = (props) => {
             <div className="Profile-subContainer u-textCenter">
               <h4 className="Profile-subTitle">About Me</h4>
               <div id="profile-description">
-                I am really allergic to cats i don't know why i have a catbook
+                I am really allergic to cats I don't know why I have a catbook
               </div>
-            </div>
-            <div className="Profile-subContainer u-textCenter">
-              <h4 className="Profile-subTitle">Cat Happiness</h4>
-              <CatHappiness catHappiness={catHappiness} />
             </div>
             <div className="Profile-subContainer u-textCenter">
               <h4 className="Profile-subTitle">My Favorite Type of Cat</h4>
